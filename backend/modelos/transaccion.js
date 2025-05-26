2eb95a4241ec529aed43e6d9f0257ad204639a0d
PalafoxManuel <136537184+PalafoxManuel@users.noreply.github.com>
2025-05-25 18:17:26 -0700

    ACENTO

--- backend/modelos/transaccion.js
+++ backend/modelos/transaccion.js
@@ -4,7 +4,7 @@ const transaccionSchema = new mongoose.Schema({
   cuentaId:  { type: mongoose.Schema.Types.ObjectId, ref: 'Cuenta', required: true },
   sucursal:  { type: String, required: true },           // nueva propiedad
   fecha:     { type: Date,   default: Date.now },        // timestamp
-  tipo:      { type: String, enum: ['depósito','retiro'], required: true },
+  tipo:      { type: String, enum: ['deposito','retiro'], required: true },
   monto:     { type: Number, required: true }
 });
 
